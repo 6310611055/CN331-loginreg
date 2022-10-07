fbdb80c18c61232ff9129193a8e3ac26cf67cc16
6310611055 <6310611055@student.tu.ac.th>
2022-10-07 23:33:14 +0700

    update300%

--- loginreg/loginreg/settings.py
+++ loginreg/loginreg/settings.py
@@ -127,5 +127,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CSRF_TRUSTED_ORIGINS = ['http://127.0.0.1:8000/']
 
-import django_heroku
-django_heroku.settings(locals())
+"""import django_heroku
+django_heroku.settings(locals())"""
